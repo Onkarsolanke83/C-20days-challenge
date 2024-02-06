de9b638da4d88baf7d5c8acff63cb0a3606a6b0f
Onkarsolanke83 <onkarsolanke83@gmail.com>
2024-02-06 22:40:04 +0530

    adding area of circle

---
+++ areaofcircle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float r; //radius of circle
+    cout<<"Enter Radius: ";
+    cin>>r;
+    float a = 3.14*r*r;
+    cout<<" Area is : ";
+    cout<<a;
+return 0;
+}
