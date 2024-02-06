d754d3fa9d7e0b2f756cdf2471158081ab6dcdfa
Onkarsolanke83 <onkarsolanke83@gmail.com>
2024-02-07 02:11:11 +0530

    removing files

--- First.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout<<"DAY 1"<<endl;
-    cout<<"starting a 20 days of code challenge"<<"\n"<<"by Onkar";
-    return 0;
-}
--- printingno.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-   cout<<4<<endl; //printing direct no
-   cout<<4*10; //multiplying no
-   cout<<endl;
-   cout<<4/2; //dividing
-    return 0;
-}
