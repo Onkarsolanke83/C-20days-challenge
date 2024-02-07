bcead45359ef0901933c5ee4437769f96a574355
Onkarsolanke83 <onkarsolanke83@gmail.com>
2024-02-08 03:20:00 +0530

    if else added

---
+++ if_else/even_odd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout<<"Enter a no: ";
+    cin>>a;
+    if(a%2==0)
+    cout<<"this is even no";
+if(a%2!=0)
+    cout<<"this is odd no";
+return 0;
+}
