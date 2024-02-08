ea3a9a536810d0951a6b5f90c8b3f53a335a3655
Onkarsolanke83 <onkarsolanke83@gmail.com>
2024-02-09 01:06:30 +0530

    add ../greatest3.cpp

---
+++ if_else/greatest3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cout<<"Enter a no a: ";
+    cin>>a;
+    int b;
+    cout<<"Enter a no b: ";
+    cin>>b;
+    int c;
+    cout<<"Enter a no c: ";
+    cin>>c;
+    if(a>b && a>c){
+         cout<<a<<"is greatest";
+    }
+  else  if(b>a && a>c){
+         cout<<b<<"is greatest";
+    }
+    else {
+                 cout<<c<<"is greatest";
+    }
+
+return 0;
+}
