85eac98943abde0cddfa8f616996b74bdf951909
Onkarsolanke83 <onkarsolanke83@gmail.com>
2024-02-29 19:40:38 +0530

    adding star printing filee

---
+++ LOOP/starprinting/square star.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+   
+
+        for( int i=1; i>7; i++){
+            for( int j=1; j>7-i+1; j++) {
+                cout<<i;
+            }
+            cout<<endl;
+        }
+
+
+
+}
